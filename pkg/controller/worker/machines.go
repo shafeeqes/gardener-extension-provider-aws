b8ebf0f43a532fc1636040db5f7270c3a5d4553f
Shafeeque E S <shafeeque.e.s@sap.com>
2024-10-06 18:26:25 +0530

    wip

--- pkg/controller/worker/machines.go
+++ pkg/controller/worker/machines.go
@@ -30,6 +30,7 @@ import (
 	"github.com/gardener/gardener-extension-provider-aws/charts"
 	awsapi "github.com/gardener/gardener-extension-provider-aws/pkg/apis/aws"
 	awsapihelper "github.com/gardener/gardener-extension-provider-aws/pkg/apis/aws/helper"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 )
 
 var (
@@ -222,7 +223,12 @@ func (w *workerDelegate) generateMachineConfig(ctx context.Context) error {
 				deploymentName          = fmt.Sprintf("%s-%s-z%d", w.worker.Namespace, pool.Name, zoneIndex+1)
 				className               = fmt.Sprintf("%s-%s", deploymentName, workerPoolHash)
 				awsCSIDriverTopologyKey = "topology.ebs.csi.aws.com/zone"
+				updateStrategy          = gardencorev1beta1.RollingUpdate
 			)
+
+			if pool.UpdateStrategy != nil {
+				updateStrategy = *pool.UpdateStrategy
+			}
 
 			machineDeployments = append(machineDeployments, worker.MachineDeployment{
 				Name:           deploymentName,
@@ -239,6 +245,7 @@ func (w *workerDelegate) generateMachineConfig(ctx context.Context) error {
 				Taints:                       pool.Taints,
 				MachineConfiguration:         genericworkeractuator.ReadMachineConfiguration(pool),
 				ClusterAutoscalerAnnotations: extensionsv1alpha1helper.GetMachineDeploymentClusterAutoscalerAnnotations(pool.ClusterAutoscaler),
+				UpdateStrategy:               updateStrategy,
 			})
 
 			machineClassSpec["name"] = className
